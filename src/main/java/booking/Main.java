c9b3b7ddf0cd8ab94ad903f94cb69057eb9c1fe0
IzzatRamazanli <86556311+IzzatRamazanli@users.noreply.github.com>
2022-09-18 13:05:29 +0400

    in progress

--- src/main/java/booking/Main.java
+++ src/main/java/booking/Main.java
@@ -1,7 +1,5 @@
 package booking;
 
-
-
 public class Main {
     public static void main(String[] args) {
         BookingApp app = new BookingApp();
--- src/main/java/booking/commands/MakeBooking.java
+++ src/main/java/booking/commands/MakeBooking.java
@@ -8,10 +8,7 @@ import booking.model.Flight;
 import booking.model.Passenger;
 import booking.model.User;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
-import java.util.Scanner;
+import java.util.*;
 
 public class MakeBooking {
     private final BookingController controllerB;
@@ -29,19 +26,20 @@ public class MakeBooking {
 
 
     public void makeBooking(User user) {
-        Flight flight = getFlight();
+        Flight flight = getFlight(user);
         if (flight != null) {
-            List<Passenger> passengers = new ArrayList<>(flight.getPassengers());
-            addPassengers(flight);
+            List<Passenger> passengers = new ArrayList<>(addPassengers(flight));
             Booking booking = new Booking(bookingID++, user, flight, passengers);
-            controllerB.doReservation(booking);
+            if (controllerB.doReservation(booking)) {
+                c.print("\nReservation completed, have a good journey");
+            }
         } else {
             c.print("Flight to corresponded destination is not exist\n");
             makeBooking(user);
         }
     }
 
-    private void addPassengers(Flight flight) {
+    private List<Passenger> addPassengers(Flight flight) {
         c.print("Enter passengers count for reserving seats: ");
         int seats = getNumber();
         if (seats > 0) {
@@ -51,6 +49,7 @@ public class MakeBooking {
                 flight.addPassenger(p);
             }
         }
+        return flight.getPassengers();
     }
 
     private Passenger passengerInfo(int i) {
@@ -72,13 +71,13 @@ public class MakeBooking {
 
     }
 
-    public Flight getFlight() {
+    public Flight getFlight(User user) {
         List<Flight> flights = this.flight.searchFlight();
         flights.forEach(System.out::println);
         if (flights.size() > 0) {
-            Flight f = selectFlight(flights);
+            Flight f = selectFlight(flights, user);
             if (f == null) {
-                return getFlight();
+                return getFlight(user);
             } else {
                 return f;
             }
@@ -86,11 +85,16 @@ public class MakeBooking {
         return null;
     }
 
-    public Flight selectFlight(List<Flight> flights) {
+    public Flight selectFlight(List<Flight> flights, User user) {
         c.print("\nSelect flight from menu(Enter flight ID): ");
         int id = getNumber();
         Optional<Flight> find = flights.stream().filter(x -> x.id() == id).findFirst();
         if (id > 0 && find.isPresent()) {
+            Optional<Booking> find2 = user.getBookings().stream().filter(x -> x.flight().id() == id).findAny();
+            if (find2.isPresent()) {
+                c.print("Reservation for this flight already exist!\n");
+                return null;
+            }
             return controllerF.getFlight(id);
         } else {
             c.print("Entered ID is not exist on menu, try again!\n");
